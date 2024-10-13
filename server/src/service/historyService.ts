9fa078e49f706d260af8ced79dc0def761629e42
Jbase16 <jphil16@icloud.com>
2024-10-13 03:49:23 -0700

    Implement read and write methods in HistoryService for managing city data

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -27,11 +27,24 @@ class HistoryService {
 
 // TODO: Define a read method that reads from the searchHistory.json file
 
-private
-
+private async read(): Promise<City[]> {
+  try {
+    const data = await fs.readFile(this.filePath, 'utf8');
+    return JSON.parse(data);
+  } catch (error) {
+    if ((errir as NodeJS.ErrnoException).code === 'ENOENT') {
+      return [];
+    }
+    throw error;    
+  }
+}
 
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
-  // private async write(cities: City[]) {}
+
+    private async write(cities: City[]): Promise<void> {
+    await fs.writeFile(this.filePath, JSON.stringify(cities, null, 2));
+  }
+  
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
   // async getCities() {}
   // TODO Define an addCity method that adds a city to the searchHistory.json file
