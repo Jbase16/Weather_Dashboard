21e3817bcb2326d60842a23da77bf4989b2cdd47
Jbase16 <jphil16@icloud.com>
2024-10-13 04:35:27 -0700

    Clean up HistoryService by removing unnecessary whitespace and comments

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -1,4 +1,3 @@
-
 import fs from 'fs/promises';
 import path from 'path';
 
@@ -6,40 +5,18 @@ class City {
   id: string;
   name: string;
 
-
-
   constructor(name: string) {
     this.id = Date.now().toString();
     this.name = name;
-
   }
 }
-
-
-
-
-
 
 class HistoryService {
   private filePath: string;
 
-
   constructor() {
-
     this.filePath = path.join(__dirname, '..', '..', 'data', 'searchHistory.json');
   }
-
-
-
-
-
-
-
-
-
-
-
-
 
   private async read(): Promise<City[]> {
     try {
@@ -51,25 +28,15 @@ class HistoryService {
       }
       throw error;
     }
-
   }
-
-
-
-
 
   private async write(cities: City[]): Promise<void> {
     await fs.writeFile(this.filePath, JSON.stringify(cities, null, 2));
   }
 
-
-
   async getCities(): Promise<City[]> {
     return this.read();
   }
-
-
-
 
   async addCity(cityName: string): Promise<void> {
     const cities = await this.read();
@@ -78,18 +45,11 @@ class HistoryService {
     await this.write(cities);
   }
 
-
-
-
   async removeCity(cityId: string): Promise<void> {
     let cities = await this.read();
-
     cities = cities.filter(city => city.id !== cityId);
     await this.write(cities);
   }
-
-
 }
-
 
 export default new HistoryService();
