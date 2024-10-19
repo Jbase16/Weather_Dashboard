1ad242d2b0c68720bf55e0a9678a1b1dfeb6e7d0
Jbase16 <jphil16@icloud.com>
2024-10-18 19:49:35 -0700

    Refactor server setup: import necessary modules, configure dotenv, and set up static file serving

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,28 @@
-import dotenv from 'dotenv';
-import express from 'express';
-dotenv.config();
+import express from "express";
+import fs from "fs";
+import path from "path";
+import axios from "axios";
+import dotenv from "dotenv";
+import { v4 as uuidv4 } from "uuid";
 
-// Import the routes
-import routes from './routes/index.js';
+dotenv.config(); // Load environment variables from .env file
 
 const app = express();
-
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../../client')));
+
+const searchHistoryPath = path.join(__dirname, '../db/searchHistory.json);
+
+
+// Import the routes
+// import routes from "./routes/index.js";
+
+// const app = express();
+
+// const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
 
