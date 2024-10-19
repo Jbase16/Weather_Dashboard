5317e6d0928d7e66df20a5b01066beb37da5076f
Jbase16 <jphil16@icloud.com>
2024-10-19 14:25:33 -0700

    Define Coordinates interface

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -3,6 +3,11 @@ dotenv.config();
 
 // TODO: Define an interface for the Coordinates object
 
+interface Coordinates {
+  lat: number;
+  lon: number;
+}
+
 // TODO: Define a class for the Weather object
 
 // TODO: Complete the WeatherService class
@@ -11,13 +16,18 @@ class WeatherService {
   // TODO: Create fetchLocationData method
   // private async fetchLocationData(query: string) {}
   // TODO: Create destructureLocationData method
-  // private destructureLocationData(locationData: Coordinates): Coordinates {}
+  private destructureLocationData(locationData: Coordinates): Coordinates {
+    return locationData;
+  }
   // TODO: Create buildGeocodeQuery method
   // private buildGeocodeQuery(): string {}
   // TODO: Create buildWeatherQuery method
   // private buildWeatherQuery(coordinates: Coordinates): string {}
   // TODO: Create fetchAndDestructureLocationData method
-  // private async fetchAndDestructureLocationData() {}
+  private async fetchAndDestructureLocationData(query: string): Promise<Coordinates> {
+    const locationData: Coordinates = await this.fetchLocationData(query);
+    return this.destructureLocationData(locationData);
+  }
   // TODO: Create fetchWeatherData method
   // private async fetchWeatherData(coordinates: Coordinates) {}
   // TODO: Build parseCurrentWeather method
